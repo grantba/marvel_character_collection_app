867714e86c65ce9c21d2282f6bb86551da55ca7d
Blaire <grant.blaire@gmail.com>
2021-06-27 07:04:55 -0500

    Added consts to index.js file for Marvel API attribution as well as pulling elements from the index.html file for DOM manipulation.

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const _marvelCopyright = "© 2021 MARVEL";
+const _attributionText = "Data provided by Marvel. © 2021 MARVEL";
+const _attributionHTML = "<a href=\"http://marvel.com\">Data provided by Marvel. © 2021 MARVEL</a>";
+const _baseUrl = "http://gateway.marvel.com/v1/public/characters";
+
+const sideNav = document.querySelector(".sidenav");
+const characterDiv = document.getElementById("character-container");
+const commentsDiv = document.getElementById("comments-container");
+const header = document.getElementById("header");
+const contentArea = document.getElementById("content");
