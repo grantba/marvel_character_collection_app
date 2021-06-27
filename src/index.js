24205c80fa861d3538a46c4ecf428df8c9c6d9dd
Blaire <grant.blaire@gmail.com>
2021-06-27 07:13:35 -0500

    Added addHeaderContent function to index.js file to load header on page load. Added addSideNavContent function to index.js file to load side nav bar on page load. Will update this later to only add side nav bar once user logs in successfully. Added addCharacterSearchForm function to index.js file to allow user to search for Marvel characters via the Marvel API once the search characters link is clicked.

--- src/index.js
+++ src/index.js
@@ -8,3 +8,42 @@ const characterDiv = document.getElementById("character-container");
 const commentsDiv = document.getElementById("comments-container");
 const header = document.getElementById("header");
 const contentArea = document.getElementById("content");
+
+function addHeaderContent() {
+    const headerContent = `
+        <a href="#" class="logo">Welcome to the Marvel Characters App</a>
+        <div class="header-right">
+        <a id="search-characters" href="#">Search Characters</a>
+        <a href="#" id="login-btn">Login</a>
+        <a href="#"id="signup-btn">Signup</a>
+        </div>
+    `
+    header.innerHTML = headerContent;
+
+    const searchCharacters = document.getElementById("search-characters");
+    searchCharacters.addEventListener("click", addCharacterSearchForm);
+}
+
+addHeaderContent();
+
+function addSideNavContent() {
+    const sideNavContent = `
+        <a href="#" id="my-character-collection">My Character Collection</a>
+        <a href="#" id="my-comments">My Comments</a>
+        <!-- <a href="#" id="projects">Projects</a>
+        <a href="#" id="contact">Contact</a> -->
+    `
+    sideNav.innerHTML = sideNavContent;
+}
+
+addSideNavContent();
+
+function addCharacterSearchForm() {
+    if (commentsDiv.innerHTML === "" && characterDiv.innerHTML === "") {
+        displaySearchForm();
+    }
+    else {
+        characterDiv.innerHTML = "";
+        commentsDiv.innerHTML = "";
+    }
+}
